31d86f156affe8d71cfcffcdb512cd03560ba800
heyscottn <heyscottn@gmail.com>
2026-07-30 12:41:00 -0500

    mm

--- 311print/learn.py
+++ 311print/learn.py
@@ -1,100 +1,32 @@
-print("\n\n\n")
-
 # learning about the print() function
 print("Hello world!")
 
 
-print("\n\n\n")
-print('hello again')
-print("it's a great time to code!") # correct
-#print('it's a great time to code!')  # error!
-
-
+# single quotes or double quotes?
 print("\n\n\n")
 
 
-print("this", "is", "a", "strange", "way")
-print("this is easier")
 
+# printing multiple items
 print("\n\n\n")
 
 
-print("    sometimes     we    need    space")
-print("python has several 'special' characters")
-print("\t will print a tab")
-print("try \t\t\t this")
 
-
+# spaces, do they matter or not??
 print("\n\n\n")
 
 
 
 
-print("this could be helpful for creating columns")
-print("age \t height in inches")
-print("1 \t\t 21")
-print("2 \t\t 25")
-print("3 \t\t 29")
-print("4 \t\t 36")
-
-
+# tabs and newlines
 print("\n\n\n")
 
 
-print("hello")
-print  ("hello")
-print(    "hello"    )
-print("h e l l o")
 
 
+# ways to add comments
 print("\n\n\n")
 
-print("...starting part 3...")
-print()
-print()
-print("where am i?")
-print("\n will print a new line")
-print("and \n\n\n this")
 
 
-
-
-
-
-
-
-print("\n\n\n")
-
-# this is a comment
-# it will not affect the execution or output
-
-'''
-here is another way to create comments
-begin (and end) with triple quotes
-that's a single quote, 3 times
-everything in between is not executable code
-'''
-
-
-
-print("hello")
-print()
-print("world")
-
-
-print("hello\n")
-#print(\n)          
-print("world")
-
-
-print("hello\n")
-print("\n")
-print("world")
-
-
-'''
-print("hello")
-print("\n")
-print("world")
-'''
 
